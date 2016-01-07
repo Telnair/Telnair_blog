7d5d4363651325c3fcddc4b7d3e6f9d25cc2fd8e
Telnair <telnairworld@gmail.com>
2016-01-07 10:48:47 +0200

    changes to articles limit and icon

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.all.limit(7).order("created_at DESC")
+    @articles = Article.all.limit(6).order("created_at DESC")
     @work = Work.last
   end
 end
