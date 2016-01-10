3f0516b26fbbe822080cfbe00cb002fc371b463e
Telnair <telnairworld@gmail.com>
2016-01-10 09:36:01 +0200

    add validations and fixed code tag adaptivity

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
   belongs_to :category
+  validates :name, :content, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,4 @@
 class Work < ActiveRecord::Base
+  validates :name, :description, :image, :thumbnail, presence: true
+  validates :name, uniqueness: true
 end
